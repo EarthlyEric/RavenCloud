a39adcc479da291eb8ad130faae3ed46e15f3524
EarthlyEric <minecaeft.creeper.eric@gmail.com>
2022-05-03 23:18:24 +0800

    部分登入系統實作(尚未連接sqllite)

--- Flask_WebApp_Service.py
+++ Flask_WebApp_Service.py
@@ -1,12 +1,14 @@
 from gevent import monkey; monkey.patch_all()
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 from gevent.pywsgi import WSGIServer
 from flask_compress import Compress
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, current_user, login_required, logout_user 
 from threading import Thread
 import lib.config as config
 import lib.logger as logger
+from lib.User import User
 
+users = {'admin': {'password': 'password'}}  
 
 app = Flask(__name__,
             static_url_path='', 
@@ -14,23 +16,65 @@ app = Flask(__name__,
             template_folder='assets')
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.secret_key = 'YourKey'  
+
+login_manager = LoginManager(app)  
 
 compress = Compress()
 compress.init_app(app)
+
+@login_manager.user_loader  
+def user_loader(username):  
+    if username not in users:  
+        return  
+  
+    user = User()  
+    user.id = username  
+    return user  
+  
 
 @app.route('/')
 def index():
     logger.logger_printer(level='info',msg=f'Visitor IP:{request.remote_addr},Request Home Page.')
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    logger.logger_printer(level='info',msg=f'Visitor IP:{request.remote_addr},Request Login Page.')
-    return render_template('login.html')
+    if request.method == 'GET':
+        logger.logger_printer(level='info',msg=f'Visitor IP:{request.remote_addr},Request Login Page.')
+
+        return render_template('login.html')
+    
+        
+    username = request.form['username']  
+    if request.form['password'] == users[username]['password']:  
+        #  實作User類別  
+        user = User()  
+        #  設置id就是email  
+        user.id = username  
+        #  這邊，透過login_user來記錄user_id，如下了解程式碼的login_user說明。  
+        login_user(user)  
+        #  登入成功，轉址  
+        return redirect(url_for('dashboard'))  
+
+
+
+    return 'Bad login'
+    
+@app.route('/dashboard')  
+@login_required  
+def dashboard():     
+    if current_user.is_active:  
+        return 'Logged in as: ' + current_user.id + 'Login is_active:True'
+ 
+  
+@app.route('/logout')  
+def logout():  
+    logout_user()  
+    return 'Logged out'    
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
 
---
+++ lib/User.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+
+
+class User(UserMixin):  
+    """  
+    如果我們希望可以做更多判斷，
+    如is_administrator也可以從這邊來加入 
+    """   
+    pass 
